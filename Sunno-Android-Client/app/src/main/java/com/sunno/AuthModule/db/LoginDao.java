55a0548ddd7080204da04e17f87b2507de81d036
safeer2978 <safeerkhan2978@gmail.com>
2020-08-21 17:38:19 +0530

    fix: commit changes

---
+++ Sunno-Android-Client/app/src/main/java/com/sunno/AuthModule/db/LoginDao.java
@@ -0,0 +1,26 @@
+package com.sunno.AuthModule.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.sunno.AuthModule.data.model.User;
+
+import java.util.concurrent.Future;
+
+@Dao
+public interface LoginDao {
+
+
+    @Insert
+    long insertUser(User user);
+
+    @Delete
+    void dropTable(User user);
+
+    @Query("select * from login_database")
+    User getUser();
+
+
+}
